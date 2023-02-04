642c56a332c503b0d6f396ea8db80f2baca7f383
Michael <michaelsupercode@gmail.com>
2023-02-04 19:13:12 +0100

    initial set

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Effect from './Components/effect';
+
+function App() {
+  return (
+    <div className="App" >
+      <Effect />
+    </div>
+  );
+}
+
+export default App;
